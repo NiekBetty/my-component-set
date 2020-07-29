6218090610e2b48ec7f9bcff38a72fe806c53867
Michel Ryan Dolstra <michel.dolstra@bettyblocks.com>
2020-07-29 14:09:34 +0200

    add helper to the meContainer

--- src/components/meContainer.js
+++ src/components/meContainer.js
@@ -1,8 +1,17 @@
 (() => ({
-  name: 'MeContainer',
-  type: 'BODY_COMPONENT',
+  name: "MeContainer",
+  type: "BODY_COMPONENT",
   allowedTypes: [],
-  orientation: 'HORIZONTAL',
-  jsx: (<h1 className={classes.root}>profile: {options.authProfile}</h1>),
-  styles: B => ({ typography }) => ({}),
+  orientation: "HORIZONTAL",
+  jsx: (
+    <div>
+      <h1 className={classes.root}>profile: {options.authProfile}</h1>
+      {(() => {
+        const { useMe } = B;
+        const { data, loading, error } = useMe(options.authProfile);
+        console.log({ data, loading, error });
+      })()}
+    </div>
+  ),
+  styles: (B) => ({ typography }) => ({}),
 }))();
