0e471f4628bba8d0e673f2d75d91b638607b2aed
Michel Ryan Dolstra <michel.dolstra@bettyblocks.com>
2020-07-29 16:30:46 +0200

    make it work again

--- src/components/meContainer.js
+++ src/components/meContainer.js
@@ -6,11 +6,6 @@
   jsx: (
     <div>
       <h1 className={classes.root}>profile: {options.authProfile}</h1>
-      {(() => {
-        const { useMe } = B;
-        const { data, loading, error } = useMe(options.authProfile);
-        console.log({ data, loading, error });
-      })()}
     </div>
   ),
   styles: (B) => ({ typography }) => ({}),
